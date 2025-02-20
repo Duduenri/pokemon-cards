5bac20a6701d13d04e5a233bd0dbf4b379266bea
Duduenri <eduess290799@gmail.com>
2025-02-19 21:08:50 -0300

    :bricks: adapting the infrastructure to receive the list of cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Header from './components/Header'
+import Header from './components/Header';
+import PokemonList from './components/PokemonList';
 
 function App() {
   return (
     <>
       <Header />
+      <PokemonList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
