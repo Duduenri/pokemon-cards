271517f12c7f5eb40a6263e6615388da5bca8353
Duduenri <eduess290799@gmail.com>
2025-02-21 13:19:15 -0300

    :building_construction: finishing the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import PokemonList from './components/PokemonList';
 import SearchBar from './components/SearchBar';
 import pikachuIcon from './assets/pikachu-icon.svg';
+import RareCardsPage from './pages/RareCardsPage';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
 
   return (
-    <div className="container mx-auto px-4 flex flex-col items-center">
-      <Header setMenuOpen={setMenuOpen} />
-      <div className="mt-2 flex flex-col items-center">
-        <img src={pikachuIcon} alt="Pikachu Icon" className="w-24 h-24 mb-1" />
-        <SearchBar setSearchTerm={setSearchTerm} />
-        <PokemonList searchTerm={searchTerm} />
+    <Router>
+      <div className="container mx-auto px-4 flex flex-col items-center">
+        <Header setMenuOpen={setMenuOpen} />
+        <Routes>
+          <Route path="/" element={
+            <div className="mt-2 flex flex-col items-center">
+              <img src={pikachuIcon} alt="Pikachu Icon" className="w-24 h-24 mb-1" />
+              <SearchBar setSearchTerm={setSearchTerm} />
+              <PokemonList searchTerm={searchTerm} />
+            </div>
+          } />
+          <Route path="/rare-cards" element={<RareCardsPage />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface NavigationMenuProps {
   menuOpen: boolean;
@@ -8,9 +9,8 @@ const NavigationMenu: React.FC<NavigationMenuProps> = ({ menuOpen }) => {
   return (
     <nav className={`${menuOpen ? 'block bg-gray-700' : 'hidden'} md:flex bg-gray-800 md:bg-transparent`}>
       <ul className="flex space-x-4 md:flex-row md:space-x-4 md:space-y-0 flex-col space-y-4">
-        <li><a href="#" className="hover:text-gray-400">All Cards</a></li>
-        <li><a href="#" className="hover:text-gray-400">Promo Cards</a></li>
-        <li><a href="#" className="hover:text-gray-400">Secret/Rare Cards</a></li>
+        <li><Link to="/" className="hover:text-gray-400">All Cards</Link></li>
+        <li><Link to="/rare-cards" className="hover:text-gray-400">Rare Cards</Link></li>
       </ul>
     </nav>
   );
