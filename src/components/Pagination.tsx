55802530cce53f9bbae8b50dfef465a34a169de2
Duduenri <eduess290799@gmail.com>
2025-02-20 23:09:55 -0300

    :lipstick: styling pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -30,7 +30,8 @@ const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage, onPage
         <button
           key={index}
           onClick={() => typeof page === 'number' && onPageChange(page)}
-          className={`px-4 py-2 m-1 rounded ${currentPage === page ? 'bg-gray-800 text-gray-200' : 'bg-gray-200 text-gray-800'}`}
+          className={`px-4 py-2 m-1 rounded border-2 ${currentPage === page ? 'text-white border-gray-800' : 'text-[#2E8B57] border-white'}`}
+          style={{ backgroundColor: 'var(--color-gray-800)', color: currentPage === page ? '#FFFFFF' : '#2E8B57', borderColor: '#FFFFFF' }}
           disabled={page === '...'}
         >
           {page}
